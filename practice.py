5afc64a111e08013a72fbc23c425e445b68e9864
sayak53 <sayakchakraborty928@gmail.com>
2025-01-01 19:44:14 +0530

    simple program to check

--- practice.py
+++ practice.py
@@ -1,6 +1,15 @@
-a=56
+#1st program
+"""a=56
 b='90'
 d=int(b)
 c= a+d
-print(c)
+print(c)"""
 
+#2nd program
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+
+if(a>b):
+    print('Greater number is',a)
+else:
+    print("Gratest number is",b)    
