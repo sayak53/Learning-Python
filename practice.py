dd02e0cc1c0ade60a493f451dc4a87fcc8df2715
sayak53 <sayakchakraborty928@gmail.com>
2025-01-02 13:09:54 +0530

    practised all the programs related to conditional-statement

--- practice.py
+++ practice.py
@@ -30,3 +30,23 @@ print(e)
 print(f)"""
 
 #4th program
+"""a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=int(input("Enter 3rd number:"))
+
+if(a>b):
+    if(a>c):
+        print('Largest number is',a)
+    else:
+        print('largest is',c)
+elif(b>c):
+    print('largets is',b)
+else:
+    print('largest is',c)  """   
+
+#5th program
+"""a=int(input('enter a number'))
+if(a%7==0):
+    print('divided by 7')
+else:
+    print('not divided by 7')"""
